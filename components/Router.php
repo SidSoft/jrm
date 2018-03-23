2d61a1a787dacee95f97cac81d9240eca21b6ce6
SidSoft <32958143+SidSoft@users.noreply.github.com>
2018-03-23 10:43:01 +0200

    Update Router.php

--- components/Router.php
+++ components/Router.php
@@ -15,7 +15,7 @@ class Router {
 	/*
 	 * Return request string
 	 */
-	private function getURI(): string {
+	private function getURI() {
 		if (!empty($_SERVER['REQUEST_URI'])) {
 			return trim($_SERVER['REQUEST_URI'], '/');
 		}
